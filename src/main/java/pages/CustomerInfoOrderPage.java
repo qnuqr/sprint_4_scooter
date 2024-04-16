94b4c9f385359b0b10bd3a14348396867b2f8bef
Anuar <anuar.orynbassar@gmail.com>
2024-04-16 19:42:42 +0500

    add scooter test in dev

--- src/main/java/pages/CustomerInfoOrderPage.java
+++ src/main/java/pages/CustomerInfoOrderPage.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Arrays;
-import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class CustomerInfoOrderPage {
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,6 @@
 package pages;
 
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
